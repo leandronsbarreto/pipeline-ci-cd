5132c76aa8e6bc6d51e64d799d6e5ed62fb8d84e
Leandro Barreto <leandrobarreto@gmx.de>
2025-03-13 01:53:36 +0100

    Update calculations.test.js

--- test/calculations.test.js
+++ test/calculations.test.js
@@ -1,18 +1,18 @@
-const assert = require('chai').assert;
+import { assert } from 'chai';
 
 describe('Calculation Tests', () => {
   it('should add numbers correctly', () => {
     const result = 2 + 2;
-    assert.strictEqual(result, 4); // Esse teste vai passar
+    assert.strictEqual(result, 4); // Passes
   });
 
   it('should multiply numbers correctly', () => {
     const result = 2 * 3;
-    assert.strictEqual(result, 6); // Esse teste vai passar
+    assert.strictEqual(result, 6); // Passes
   });
 
   it('should fail on incorrect calculation', () => {
     const result = 2 + 3;
-    assert.strictEqual(result, 6); // Esse teste vai falhar
+    assert.strictEqual(result, 6); // Fails
   });
 });
